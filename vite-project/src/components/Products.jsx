bd3ab50558b3d4ecc69b868c08869c27de55681a
Saailii <ilias.taboada@gmail.com>
2024-04-29 23:31:17 +0200

    Update Products.jsx

--- vite-project/src/components/Products.jsx
+++ vite-project/src/components/Products.jsx
@@ -20,26 +20,25 @@ const Products = () => {
   }, []); // Add empty dependency array to run effect only once
 
   return (
-    <div className="h-screen w-full grid grid-cols-2 py-16">
+    <div className="h-screen w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-16 ">
       {products.map((product) => (
         <div
           key={product._id}
-          className="flex flex-col justify-center items-start  px-2   "
+          className="flex flex-col justify-center items-start px-2 md:justify-center md:items-start   py-6 "
         >
           <Link
             to={`/${product._id}`}
-            className=" flex justify-start items-start "
+            className=" flex justify-start items-start size-full "
           >
             <img
               src={`http://77.37.122.120:3001/${product.imageUrl[0]}`}
-              alt=""
-              className="w-full  relative rounded-md "
+              className="w-full rounded-md md:h-4/6"
             />
           </Link>
-          <h1 className="uppercase font-sans text-md">
+          <h1 className="uppercase font-sans text-md text-xl">
             T-Shirt Rose Matière bien
           </h1>
-          <h2 className="  font-mono">{product.price}€</h2>
+          <h2 className="  font-mono text-xl">{product.price}€</h2>
         </div>
       ))}
     </div>
